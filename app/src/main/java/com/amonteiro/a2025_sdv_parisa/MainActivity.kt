028392da99678ea22e1c6b16999eb4b8ec4112d0
Anthony <anth06ny@gmail.com>
2025-02-06 17:04:25 +0100

    Fin TP Compose affichage

--- app/src/main/java/com/amonteiro/a2025_sdv_parisa/MainActivity.kt
+++ app/src/main/java/com/amonteiro/a2025_sdv_parisa/MainActivity.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.amonteiro.a2025_sdv_parisa.ui.screens.SearchScreen
 import com.amonteiro.a2025_sdv_parisa.ui.theme._2025_sdv_parisaTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,9 +31,41 @@ class MainActivity : ComponentActivity() {
         setContent {
             _2025_sdv_parisaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SearchScreen(modifier = Modifier.padding(innerPadding))
+                    //SearchScreen(modifier = Modifier.padding(innerPadding))
+                    TestShareRemember()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun TestShareRemember() {
+    var expanded = remember { mutableStateOf(false) }
+
+    Column {
+        ElevatedButton(
+            onClick = { expanded.value = !expanded.value }
+        ) {
+            Text(if (expanded.value) "Show less" else "Show more")
+        }
+
+        ElevatedButton(
+            onClick = { expanded.value = !expanded.value },
+        ) {
+            Text(if (expanded.value) "Show less" else "Show more")
+        }
+        MyButton(expanded)
+        MyButton()
+    }
+}
+//Permet d'écouter l'état en dehors de la méthode
+@Composable
+fun MyButton(expanded: MutableState<Boolean> = remember { mutableStateOf(false) }) {
+
+    ElevatedButton(
+        onClick = { expanded.value = !expanded.value },
+    ) {
+        Text(if (expanded.value) "Show less" else "Show more")
     }
 }
--- app/src/main/java/com/amonteiro/a2025_sdv_parisa/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/amonteiro/a2025_sdv_parisa/ui/screens/SearchScreen.kt
@@ -2,19 +2,33 @@ package com.amonteiro.a2025_sdv_parisa.ui.screens
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -27,21 +41,88 @@ import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
 
 @Composable
-fun SearchScreen(modifier:Modifier = Modifier, mainViewModel: MainViewModel = MainViewModel()) {
-    Column (modifier= modifier.fillMaxSize()) {
+fun SearchScreen(modifier: Modifier = Modifier, mainViewModel: MainViewModel = MainViewModel()) {
+    Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
 
-        mainViewModel.dataList.collectAsStateWithLifecycle().value.forEach {
-            PictureRowItem(data = it)
+        var list = mainViewModel.dataList.collectAsStateWithLifecycle().value
 
+        SearchBar()
+
+        LazyColumn(modifier= Modifier.weight(1f), verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            items(list.size) {
+                PictureRowItem(data = list[it])
+            }
+        }
+
+        Row {
+
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Filled.Clear,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text(stringResource(R.string.bt_clear))
+            }
+
+            Button(
+                onClick = { /* Do something! */ },
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.AutoMirrored.Filled.Send,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(Modifier.size(ButtonDefaults.IconSpacing))
+                Text(stringResource(R.string.bt_load))
+            }
         }
     }
+}
+
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "", //Valeur affichée
+        onValueChange = {newValue:String -> }, //Nouveau texte entrée
+        leadingIcon = { //Image d'icone
+            Icon(
+                imageVector = Icons.Default.Search,
+                tint = MaterialTheme.colorScheme.primary,
+                contentDescription = null
+            )
+        },
+        singleLine = true,
+        label = { //Texte d'aide qui se déplace
+            Text("Enter text")
+            //Pour aller le chercher dans string.xml
+            //Text(stringResource(R.string.placeholder_search))
+        },
+        //placeholder = { //Texte d'aide qui disparait
+        //Text("Recherche")
+        //},
+
+        //keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search), // Définir le bouton "Entrée" comme action de recherche
+        //keyboardActions = KeyboardActions(onSearch = {onSearchAction()}), // Déclenche l'action définie
+        //Comment le composant doit se placer
+        modifier = modifier
+            .fillMaxWidth() // Prend toute la largeur
+            .heightIn(min = 56.dp) //Hauteur minimum
+    )
 }
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable //Composable affichant 1 PictureBean
 fun PictureRowItem(modifier: Modifier = Modifier, data: PictureBean) {
 
-    Row(modifier = modifier.fillMaxWidth().background(MaterialTheme.colorScheme.tertiaryContainer)) {
+    Row(modifier = modifier
+        .fillMaxWidth()
+        .background(MaterialTheme.colorScheme.tertiaryContainer)) {
         //Permission Internet nécessaire
         GlideImage(
             model = data.url,
@@ -55,17 +136,22 @@ fun PictureRowItem(modifier: Modifier = Modifier, data: PictureBean) {
             failure = placeholder(R.mipmap.ic_launcher),
             contentScale = ContentScale.Fit,
             //même autres champs qu'une Image classique
-            modifier = Modifier.heightIn(max = 100.dp) //Sans hauteur il prendra tous l'écran
-                .widthIn(max= 100.dp)
+            modifier = Modifier
+                .heightIn(max = 100.dp) //Sans hauteur il prendra tous l'écran
+                .widthIn(max = 100.dp)
         )
 
         Column(modifier = Modifier.padding(10.dp)) {
-            Text(text = data.title,
+            Text(
+                text = data.title,
                 style = MaterialTheme.typography.titleLarge,
-                color = MaterialTheme.colorScheme.primary )
-            Text(text = data.longText.take(20) + "...",
+                color = MaterialTheme.colorScheme.primary
+            )
+            Text(
+                text = data.longText.take(20) + "...",
                 style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onTertiaryContainer)
+                color = MaterialTheme.colorScheme.onTertiaryContainer
+            )
         }
     }
 
@@ -74,7 +160,7 @@ fun PictureRowItem(modifier: Modifier = Modifier, data: PictureBean) {
 
 
 @Preview(showBackground = true, showSystemUi = true)
-@Preview(showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, showSystemUi = true, uiMode = UI_MODE_NIGHT_YES, locale = "fr")
 @Composable
 fun SearchScreenPreview() {
     //Il faut remplacer NomVotreAppliTheme par le thème de votre application
